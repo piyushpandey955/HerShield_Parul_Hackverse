e53a6c907e061edc22f1f67ef5dcb62b9698bb1c
piyush pandey <piyush7838732397@gmail.com>
2025-02-22 13:20:05 +0530

    few changes

--- hershield/lib/main.dart
+++ hershield/lib/main.dart
@@ -1,35 +1,4 @@
-// import 'dart:async';
 
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:flutter_dotenv/flutter_dotenv.dart';
-// import 'package:hershield/services/notification_service.dart';
-// import 'wrapper.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/material.dart';
-
-// //handling bg notifs
-// Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-//   print("Handling background message: ${message.messageId}");
-// }
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(); //to initialise firebase 
-//   await dotenv.load(); //to load .env file
-//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler); //to handle bg notif
-//   NotificationService.initializeNotifClickingOnscreenWhenReceived(); //to initialse notif service
-//   FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(alert: true,badge: true,sound: true);
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//         debugShowCheckedModeBanner: false, home: Wrapper());
-//   }
-// }
 
 
 import 'dart:async';
